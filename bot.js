c63ba31684f197f29f0096dc1be6d77bd9b5f893
Computer Crafter <akash.adkri24@hotmail.com>
2019-11-11 21:24:29 -0600

    make it better

---
+++ bot.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+var questions = ['test1', 'test2', 'test3'];
+var answers = ['yes', 'ooo', 'bet'];
+
+
+
+
+client.on('ready', () => {
+    console.log('ready!');
+    client.guilds.forEach((guild) => {
+        const channel = guild.channels.find(ch => ch.name === 'current-event-quizzes');
+        /*while (true) {
+            var date = new Date()
+            if (date.getHours() === 18 && date.getMinutes() === 0) {
+                channel.send(`@everyone Current Event Quiz Released`);
+                channel.send(`DM your answer to me, CurrentEventQuizzesLord. In the DM, enter the question number you want to answer, followed by EXACTLY 1 space, followed by the answer. Make sure the answer is in all lowercase.`);
+                for (i = 0; i < array.length; i++) {
+                    channel.send((i + 1) + ": " + questions[i]);
+                }
+            }
+        }*/
+
+    });
+});
+
+
+client.on('message', message => {
+    if (message.content === '!quiz') {
+        message.channel.send(`@everyone Current Event Quiz Released`);
+        message.channel.send(`DM your answer to me, CurrentEventQuizzesLord. In the DM, enter the question number you want to answer, followed by EXACTLY 1 space, followed by the answer. Make sure the answer is in all lowercase.`);
+        for (i = 0; i < questions.length; i++) {
+            message.channel.send((i + 1) + ': ' + questions[i]);
+        }
+
+    }
+    if (message.channel.type === 'dm') {
+        var qnum = parseInt(message.content.substr(0, 1));
+        var ans = message.content.substr(2);
+        console.log(qnum);
+        console.log(ans);
+        if (ans === answers[qnum - 1]) {
+            if (!lastMessage.author.bot) {
+                message.channel.send('yes, that is correct');
+            }
+        } else {
+            if (!lastMessage.author.bot) {
+                message.channel.send('Either that is wrong, or you have entered bad input.');
+            }
+        }
+    }
+});
+
+
+
+client.login(auth.token);
