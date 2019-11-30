8114595e3f28219d2f6cf35494aed5ca8c9b83d2
Computer Crafter <akash.adkri24@hotmail.com>
2019-11-30 14:15:18 -0600

    removed fake leaderboard stuff, will replace with google leaderboard stuff soon

--- bot.js
+++ bot.js
@@ -18,39 +18,31 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.author.bot) return;
-    if (message.content === '!quiz') {
-        message.channel.send(`@everyone Current Event Quiz Released! Type !quiz in any channel to retrieve the quiz there as well.`);
-        message.channel.send(`DM your answer to me, CurrentEventQuizzesLord. In the DM, enter the question number you want to answer, followed by EXACTLY 1 space, followed by the answer. Make sure the answer is in all lowercase.`);
-        for (i = 0; i < questions.length; i++) {
-            message.channel.send((i + 1) + ': ' + questions[i]);
-        }
-
-    }
-    if (message.channel.type === 'dm' && !(message.content === '!leaderboard') && !(message.content === '!clearleaderboard') && !(message.content === '!mypoints') && !(message.content.startsWith('!score'))) {
+    if (message.channel.type === 'dm') {
         var qnum = parseInt(message.content.substr(0, 1));
         var ans = message.content.substr(2);
         if (ans === answers[qnum - 1]) {
             message.channel.send(`Yes, that is correct! You will receive a point for that.`);
-            points[message.author.username] += 1;
         } else {
             message.channel.send(`Either that is wrong, or you have entered bad input.`);
         }
     }
-    if (message.content === '!leaderboard') {
-        for (i = 0; i < qbers.length; i++) {
-            let user = qbers[i];
-            message.channel.send(user + `: ` + points[user]);
+    if (message.content === '!quiz') {
+        message.channel.send(`Quiz here! Type !help for a list of commands that I handle.`);
+        message.channel.send(`For the quiz, DM your answer to me, CurrentEventQuizzesLord. In the DM, enter the question number you want to answer, followed by EXACTLY 1 space, followed by the answer. Make sure the answer is in all lowercase.`);
+        for (i = 0; i < questions.length; i++) {
+            message.channel.send((i + 1) + ': ' + questions[i]);
         }
     }
-    if (message.content.startsWith('!score')) {
-        let user = message.content.substr(7);
-        message.channel.send(user + `: ` + points[user]);
+    if (message.content === '!help') {
+
+    }
+    if (message.content === '!leaderboard') {
+        
     }
     if (message.content === '!clearleaderboard') {
         if (message.author.id === '497237135317925898') {
-            for (i = 0; i < qbers.length; i++) {
-                let user = qbers[i];
-                points[user] = 0;
+            
             }
             message.channel.send('Leaderboard cleared!');
         } else {
