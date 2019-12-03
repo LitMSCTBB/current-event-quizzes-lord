b1ee7d48a2d22e6bf294bc89267852671b8e62e9
LitMSCTBB <41565910+LitMSCTBB@users.noreply.github.com>
2019-12-03 10:04:52 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,20 +16,28 @@ const embed = {
     },
     "fields": [
         {
-            "name": "!help",
+            "name": "cqlord help",
             "value": "uh duh"
         },
         {
-            "name": "!leaderboard",
+            "name": "cqlord quiz",
+            "value": "Retrieve the quiz"
+        },
+        {
+            "name": "cqlord leaderboard",
             "value": "Get leaderboard link"
         },
         {
-            "name": "!score <user>",
+            "name": "cqlord score <user>",
             "value": "Retrieve the score of <user>"
         },
         {
-            "name": "!setscore <user> <newscore>",
+            "name": "cqlord setscore <user> <newscore>",
             "value": "Sets the score of <user> to <newscore>"
+        },
+        {
+            "name": "cqlord clearleaderboard",
+            "value": "Resets the leaderboard"
         }
     ]
 };
@@ -45,40 +53,58 @@ client.on('ready', () => {
     console.log('uwu bet boi i b ready aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb!');
     guild = client.guilds.get('634574175238881280');
     guild.members.forEach(member => qbers.push(member.user.username));
+    client.user.setActivity("cqlord help");
 });
 
 
 client.on('message', message => {
-    if (message.author.bot) return;
     if (message.channel.type === 'dm') {
-        var qnum = parseInt(message.content.substr(0, 1));
-        var ans = message.content.substr(2);
-        if (ans === answers[qnum - 1]) {
-            message.channel.send(`Yes, that is correct! You will receive a point for that.`);
-        } else {
-            message.channel.send(`Either that is wrong, or you have entered bad input.`);
-        }
+         var qnum = parseInt(message.content.substr(0, 1));
+         var ans = message.content.substr(2);
+         if (ans === answers[qnum - 1]) {
+             message.channel.send(`Yes, that is correct! You will receive a point for that.`);
+         } else {
+             message.channel.send(`Either that is wrong, or you have entered bad input.`);
+         }
     }
-    if (message.content === '!quiz') {
-        message.channel.send(`Quiz here! Type !help for a list of commands that I handle.`);
-        message.channel.send(`For the quiz, DM your answer to me, CurrentEventQuizzesLord. In the DM, enter the question number you want to answer, followed by EXACTLY 1 space, followed by the answer. Make sure the answer is in all lowercase.`);
-        for (i = 0; i < questions.length; i++) {
-            message.channel.send((i + 1) + ': ' + questions[i]);
-        }
-    }
-    if (message.content === '!help') {
-        message.channel.send({ embed });
-    }
-    if (message.content === '!leaderboard') {
+    if (message.content.substr(0,7) === 'cqlord '); {
+        if (message.author.bot) return;
+        var command = message.content.substr(7);
         
-    }
-    if (message.content === '!clearleaderboard') {
-        if (message.author.id === '497237135317925898') {
-            message.channel.send('Leaderboard cleared!');
-        } else {
-            message.channel.send('You are not authorized to do that!');
+        
+        if (command === 'help') {
+            message.channel.send({ embed });
         }
-    }
+        if (command === 'quiz') {
+            message.channel.send(`Quiz here! Type !help for a list of commands that I handle.`);
+            message.channel.send(`For the quiz, DM your answer to me, CurrentEventQuizzesLord. In the DM, enter the question number you want to answer, followed by EXACTLY 1 space, followed by the answer. Make sure the answer is in all lowercase.`);
+            for (i = 0; i < questions.length; i++) {
+                message.channel.send((i + 1) + ': ' + questions[i]);
+            }
+        }
+        if (command.substr(0, 5) === 'score') {
+        
+        }
+        if (command.substr(0, 8) === 'setscore') {
+        
+        }
+        if (command === 'leaderboard') {
+        
+        }
+        if (command === 'clearleaderboard') {
+            if (message.author.id === '497237135317925898') {
+                
+                
+                
+                
+                
+                
+                message.channel.send('Leaderboard cleared!');
+            } else {
+                message.channel.send('You are not authorized to do that!');
+            }
+        }
+	}
 });
 
 
