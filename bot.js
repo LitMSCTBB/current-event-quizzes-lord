fee6566c45bd04da5ac5624023322f730f2cd1b3
LitMSCTBB <minecraftandcomputers@gmail.com>
2020-03-17 11:57:35 -0500

    adding several uncommitted changes from a long time ago

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
     measurementId: "G-WSPKY8G03K"
 };
 firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
+var db = firebase.database();
 
 //node-schedule
 const schedule = require('node-schedule');
@@ -27,12 +27,7 @@ const schedule = require('node-schedule');
 //Sheets API
 const {google} = require('googleapis');
 const keys = require('./keys.json');
-const client2 = new google.auth.JWT(
-    keys.client_email,
-    null,
-    keys.private_key,
-    ['https://www.googleapis.com/auth/spreadsheets']
-);
+const client2 = new google.auth.JWT(keys.client_email, null, keys.private_key, ['https://www.googleapis.com/auth/spreadsheets']);
 client2.authorize(function (err, tokens) {
     if (err) {
         console.log(err);
@@ -41,37 +36,30 @@ client2.authorize(function (err, tokens) {
         console.log('Connected with Sheets API!');
     }
 });
-//for next time: define more async functions meant for the purposes of the functions for the bot i still need to do, and figure out how to do disjoint ranges
-async function gsrun(cl) {
+async function dbToGS(cl) {
     const gsapi = google.sheets({ version: 'v4', auth: cl });
     const opt = {
         spreadsheetId: '1xq-avwsqvh4RsndyrUnVHo0dZo4-aFJs4ItNW1I5His',
-        range: 'C2:Z4'
     };
-    let entries = await gsapi.spreadsheets.values.get(opt);
-    let dataArray = entries.data.values;
-    //make sure there are always the same amount of columns to prevent unwanted undefined things in the sheet
-    dataArray = dataArray.map(function (r) {
-        while (r.length < 2) {
-            r.push('');
-        }
-        return r;
-    });
-
-    dataArray[0].push('3');
-
+    var all = db.ref();
+    var all_vals;
+    all.once('value', function (data) {
+        all_vals = data.val();
+    })
     const updateOptions = {
         spreadsheetId: '1xq-avwsqvh4RsndyrUnVHo0dZo4-aFJs4ItNW1I5His',
-        range: 'C2',
+        ranges: ['Leaderboard!A2:A1000', 'Leaderboard!C2'],
         valueInputOption: 'USER_ENTERED',
-        resource: { values: dataArray }
+        resource: { values:  all_vals}
     };
 
-    let res = await gsapi.spreadsheets.values.update(updateOptions);
+    let res = await gsapi.spreadsheets.values.batchUpdate(updateOptions);
 };
 
+
+
 //help embed
-const embed = {
+const helpEmbed = {
     "title": "Command List",
     "color": 100000,
     "footer": {
@@ -104,14 +92,6 @@ const embed = {
             "name": "cqlord score <user>",
             "value": "Retrieve the score of <user>"
         },
-        {
-            "name": "cqlord setscore <user> <newscore>",
-            "value": "Sets the score of <user> to <newscore>"
-        },
-        {
-            "name": "cqlord clearleaderboard",
-            "value": "Resets the leaderboard"
-        }
     ]
 };
 
@@ -119,12 +99,25 @@ const embed = {
 var questions = ['test1', 'test2', 'test3'];
 var answers = ['yes', 'ooo', 'bet'];
 
+var updateGSheetsRef = db.ref();
+updateGSheetsRef.on('value', function (data) {
+    dbToGS(client2);
+})
+
 client.on('ready', () => {
     console.log('uwu bet boi i b ready aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb!');
     guild = client.guilds.get('634574175238881280');
     client.user.setActivity("cqlord help");
     var j = schedule.scheduleJob({ hour: 18, minute: 0, dayOfWeek: 0 }, function () {
         client.channels.get('635672167773896725').send('Quiz released @everyone. For the quiz, DM your answer to me, CurrentEventQuizzesLord. In the DM, (all in one message) "cqlord answer", followed by EXACTLY 1 space, followed by the question number you want to answer, followed by EXACTLY 1 space, followed by the answer. Make sure the answer is in all lowercase.');
+        for (i = 0; i < questions.length; i++) {
+            client.channels.get('635672167773896725').send((i + 1) + ': ' + questions[i]);
+        }
+        var ref = db.ref('week');
+        ref.once('value', function (data) {
+            var week = data.val() + 1;
+            ref.set(week + 1);
+        });
     });
 });
 
@@ -135,10 +128,10 @@ client.on('message', message => {
         var command = message.content.substr(7);
         
         if (command === 'help') {
-            message.channel.send({ embed });
+            message.channel.send({ helpEmbed });
         }
         if (command === 'quiz') {
-            message.channel.send(`Quiz released @everyone. For the quiz, DM your answer to me, CurrentEventQuizzesLord. In the DM, (all in one message) "cqlord answer", followed by EXACTLY 1 space, followed by the question number you want to answer, followed by EXACTLY 1 space, followed by the answer. Make sure the answer is in all lowercase.`);
+            message.channel.send(`For the quiz, DM your answer to me, CurrentEventQuizzesLord. In the DM, (all in one message) "cqlord answer", followed by EXACTLY 1 space, followed by the question number you want to answer, followed by EXACTLY 1 space, followed by the answer. Make sure the answer is in all lowercase.`);
             for (i = 0; i < questions.length; i++) {
                 message.channel.send((i + 1) + ': ' + questions[i]);
             }
@@ -147,45 +140,21 @@ client.on('message', message => {
             if (message.channel.type === 'dm') {
                 var qnum = parseInt(command.substr(7, 8));
                 var ans = command.substr(9);
+                //check whether user has gotten right already
+                
                 if (ans === answers[qnum - 1]) {
                     message.channel.send(`Yes, that is correct! You will receive a point for that.`);
+
                 } else {
                     message.channel.send(`Either that is wrong, or you have entered bad input. Your answer was ` + ans + '.');
-                    
                 }
             }
         }
         if (command.startsWith('score ')) {
         
         }
-        if (command.startsWith('setscore ')) {
-            if (message.author.id === '497237135317925898') {
-
-
-
-
-
-
-                message.channel.send('Score set!');
-            } else {
-                message.channel.send('You are not authorized to do that!');
-            }
-        }
         if (command === 'leaderboard') {
             message.channel.send("https://docs.google.com/spreadsheets/d/1xq-avwsqvh4RsndyrUnVHo0dZo4-aFJs4ItNW1I5His/edit#gid=0");
-        }
-        if (command === 'clearleaderboard') {
-            if (message.author.id === '497237135317925898') {
-                
-                
-                
-                
-                
-                
-                message.channel.send('Leaderboard cleared!');
-            } else {
-                message.channel.send('You are not authorized to do that!');
-            }
         }
 	}
 });
